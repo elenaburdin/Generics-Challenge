8c338cbfb8acbd0ea40efcef9ca9d572c59c7567
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2022-09-12 19:03:56 +0300

    Add files via upload

---
+++ BaseballPlayer.java
@@ -0,0 +1,7 @@
+package com.challenge;
+
+public class BaseballPlayer extends Player{
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ FootballPlayer.java
@@ -0,0 +1,7 @@
+package com.challenge;
+
+public class FootballPlayer extends Player{
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
